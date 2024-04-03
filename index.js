7d98be6d0e00be6d2ea26fa84a999f030b63d9fc
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-04-03 23:56:38 +0530

    Solve the error in edit request...

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/posts/:id", (req, res) => {
     res.render("show.ejs", {post});
 });
 
-app.patch("/poosts/:id", (req, res) => {
+app.patch("/posts/:id", (req, res) => {
     let {id} = req.params;
     let newContent = req.body.content;
     let post = posts.find((p) => id === p.id);
